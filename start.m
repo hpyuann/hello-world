c5649da2a9cf7241522e9e701a06a60302c0b5b4
hpyuann <hpyuann@gmail.com>
2015-03-06 10:50:09 -0800

    Update start.m

--- start.m
+++ start.m
@@ -1,4 +1,4 @@
 % I didn't download anything
 % Is this a MATLAB m.file?
-fprintf('Hi, I want to graduate asap. Don't let me spend too much time to figure you out. \n')
+fprintf('Hi, I want to graduate asap. Do not let me spend too much time to figure you out. \n')
 fprintf('Anybody here? \n')
